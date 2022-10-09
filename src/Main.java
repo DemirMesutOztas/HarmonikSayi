e73020f323de34abcaca9d389d2f26dd44745e39
Gogle Öztaş <demiroztas06@hotmail.com>
2022-10-09 20:17:18 +0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        int x;
+        double result=0.0;
+        System.out.print("Sayı giriniz: ");
+        x=input.nextInt();
+
+        for (int i=1; i<=x;i++)
+        {
+            result+=(1.0/i);
+        }
+
+        System.out.print("Harmonik Değeri: "+result);
+
     }
 }
